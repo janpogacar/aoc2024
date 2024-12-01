9f52f5b4b8741e337f200b4e7bf401aab20a92ca
janpogacar <jan.pogachar@gmail.com>
2024-12-01 18:53:21 +0100

    Day 1 solution cleaned up

--- Day01/Day01.py
+++ Day01/Day01.py
@@ -1,26 +1,16 @@
 import numpy as np
 
 # parse input data
-input_data = np.loadtxt("input_01.txt", delimiter="   ", dtype="int32")
+input_data = np.loadtxt("input_01.txt", dtype="int32")
 
 t_input_data = np.transpose(input_data)
-t_input_data[0] = np.sort(t_input_data[0])
-t_input_data[1] = np.sort(t_input_data[1])
+t_input_data.sort(axis=1)
 
-result01 = 0
-
-for i,x in enumerate(t_input_data[0]):
-    result01 += abs(t_input_data[0][i] - t_input_data[1][i])  
-
+result01 = np.sum(np.abs(t_input_data[0] - t_input_data[1]))
 print(f"Part 1 solution: {result01}")
-
-# create dict of unique second column values
-unique, counts = np.unique(t_input_data[1], return_counts=True)
-s_c_values = dict(zip(unique, counts))
 
 result02 = 0
 for x in t_input_data[0]:
-    if x in t_input_data[1]: # need to check if dict value exists
-        result02 += x*s_c_values[x] 
+    result02 += x*list(t_input_data[1]).count(x)
 
 print(f"Part 2 solution: {result02}")
