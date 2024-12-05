88cc0a13e3578469300f82c9910f518f045f6339
jan.pogacar@renishaw.com <jan.pogacar@renishaw.com>
2024-12-05 11:08:05 +0100

    Day 5 solution re-implemented with cmp_to_key

--- Day05/Day05.py
+++ Day05/Day05.py
@@ -1,3 +1,8 @@
+from functools import cmp_to_key
+
+def compare(item1, item2):
+    return -1 if [item1, item2] in page_rules else 1
+
 input_file = open("input05_01.txt", "r")  
 page_rules = input_file.read().split("\n")
 for i, x in enumerate(page_rules):
@@ -9,40 +14,22 @@ for i, x in enumerate(updates):
     updates[i] = x.split(",")
 
 result_list = []
+result_list_p2 = []
 for i, x in enumerate(updates):
-    update_match = True
-    for j in range(len(x)-1):
-        if [x[j], x[j+1]] not in page_rules:
-            update_match = False
-            break
-    if update_match == True:
-        result_list.append(x)
+    sorted_x = sorted(x, key=cmp_to_key(compare))
+    if sorted_x == x:
+       result_list.append(x)
+    else:
+       result_list_p2.append(sorted_x)
 
 result = 0
 for x in result_list:
     result += int(x[int((len(x) - 1)/2)])
 
-
 print(result)
 
-result_list = []
-for i, x in enumerate(updates):
-    update_match = True
-    sorted = False
-    while(sorted == False):
-        sorted = True
-        for j in range(len(updates[i])-1):
-            if [updates[i][j], updates[i][j+1]] not in page_rules:
-                sorted = False
-                update_match = False
-                tmp = updates[i][j]
-                updates[i][j] = updates[i][j+1]
-                updates[i][j+1] = tmp
-    if update_match == False:
-        result_list.append(updates[i])
-
 result = 0
-for x in result_list:
+for x in result_list_p2:
     result += int(x[int((len(x) - 1)/2)])
 
 print(result)
